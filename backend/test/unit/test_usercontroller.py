7d8667bba7cb9fcd7ac61e90ff3d59c3b3975728
Hani Islambouli <hais22@student.bth.se>
2024-04-17 00:35:21 +0200

    Improvements

--- backend/test/unit/test_usercontroller.py
+++ backend/test/unit/test_usercontroller.py
@@ -2,6 +2,7 @@ import pytest
 from unittest.mock import Mock
 from src.controllers.usercontroller import UserController
 
+
 class TestUserController:
     @pytest.fixture
     def mock(self):
@@ -20,11 +21,20 @@ class TestUserController:
         assert str(e.value) == 'Error: invalid email address'
 
     def test_get_user_by_email_nonexistent(self, mock):
-        mock.find.return_value = none
+        mock.find.return_value = [None]
         user_controller = UserController(mock)
         user = user_controller.get_user_by_email('nonexistent@example.com')
         assert user is None, "The user does not exist"
 
     def test_get_user_by_email_multiple_users(self, mock):
-        return
+        mock.find.return_value = [{'email': 'smith@gmail.com', 'name': 'Smith'}, {'email': 'smith@gmail.com', 'name': 'John'}]
+        user_controller = UserController(mock)
+        user = user_controller.get_user_by_email('smith@gmail.com')
+        assert user == {'email': 'smith@gmail.com', 'name': 'Smith'}
 
+    def test_get_user_by_email_exception_propagation(self, mock):
+        mock.find.side_effect = Exception("Database error")
+        user_controller = UserController(mock)
+        with pytest.raises(Exception) as exc_info:
+            user_controller.get_user_by_email('test@example.com')
+        assert str(exc_info.value) == "Database error"
